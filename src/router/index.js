379fe3b333b1808d047e83509dd9b99ba8bab820
Xecades <i@xecades.xyz>
2024-08-05 22:04:07 +0800

    chore: naming scheme, index.html

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import routes from "./routes.js";
-import setupGuards from "./guards.js";
+import routes from "./routes";
+import setupGuards from "./guards";
 
 const scrollBehavior = (_, __, saved_position) =>
     saved_position ?? { left: 0, top: 0, behavior: "smooth" };
