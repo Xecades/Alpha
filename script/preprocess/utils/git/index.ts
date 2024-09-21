830290e943552684784ed12a5bad7b893597b736
Xecades <i@xecades.xyz>
2024-09-21 19:01:26 +0800

    fix: import SimpleGit as a type

--- script/preprocess/utils/git/index.ts
+++ script/preprocess/utils/git/index.ts
@@ -1,11 +1,12 @@
-import { simpleGit, SimpleGit } from "simple-git";
+import { simpleGit } from "simple-git";
+import type { SimpleGit } from "simple-git";
 
 export const timeDataOf = async (file: string) => {
     const git: SimpleGit = simpleGit();
     const commits = await git.log({ file });
 
-    const created = new Date(commits.all.at(-1)!.date);
-    const updated = new Date(commits.latest!.date);
+    const created: Date = new Date(commits.all.at(-1)!.date);
+    const updated: Date = new Date(commits.latest!.date);
 
     return { created, updated };
 };
