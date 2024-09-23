a6e7b7a01f2c2a3facabe5d335dedc03530fd191
Xecades <i@xecades.xyz>
2024-09-23 10:46:38 +0800

    fix: when file not committed, show current timestamp instead of git commit timestamp

--- script/preprocess/utils/git/index.ts
+++ script/preprocess/utils/git/index.ts
@@ -5,6 +5,11 @@ export const timeDataOf = async (file: string) => {
     const git: SimpleGit = simpleGit();
     const commits = await git.log({ file });
 
+    if (commits.total === 0) {
+        const now: string = new Date().toISOString();
+        return { created: now, updated: now };
+    }
+
     const created: string = commits.all.at(-1)!.date;
     const updated: string = commits.latest!.date;
 
